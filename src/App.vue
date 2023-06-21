59fcae3f1a63792c98024a8cfdcd4ae722508454
pauljimmyab1230 <paul.auccapuclla.27@unsch.edu.pe>
2023-06-20 22:17:21 -0500

    v2

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+    <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+            ¿Cuánto <span class="text-indigo-500">dinero</span> necesitas?
+        </h1>
+        
+    </div>
+</template>
